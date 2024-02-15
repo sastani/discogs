d772afe33a3aa7fbd2f4e025a13e0311fc77b868
sastani <sinaastani@gmail.com>
2024-02-15 01:40:48 -0800

    add title to release object; modify code to add release to queue when parser has hit end of the release

--- parser.py
+++ parser.py
@@ -6,37 +6,42 @@ def parse_xml(file_name):
     context = etree.iterparse(file_name, events=('start', 'end'))
     #event, root = next(context)
     #attributes = ["id"]
+    all_releases = deque()
     for event, element in context:
         tag = element.tag
         #if we have found "start" of release, build release object
         if event == "start" and tag == "release":
-            release = dict()
-            release_tracks = list()
-            release_labels = list()
             release_id = element.get('id')
             release_status = element.get('status')
             release = ReleaseObject(release_id, release_status)
+        #if we have found "end" of release, add release object to queue
+        elif event == "end" and tag == "release":
+            all_releases.append(release)
         else:
             if event == "start" and tag == "artists":
                 for artist in element.iterchildren():
-                    add_children(release, artist)
+                    artist_row = dict()
+                    add_children(artist_row, artist)
+                    release.get_artists().append(artist_row)
+                    #print(release.get_artists())
+            elif event == "start" and tag == "title":
+                parent = element.getparent()
+                if parent.tag == "release":
+                    release.set_title(element.text)
+                    #print(release.get_title())
             elif event == "start" and tag == "labels":
                 for label in element.iterchildren():
                     label_row = dict()
                     add_attributes(label_row, label)
-                    print(label_row)
-                #get label element
-                #event, element = next(context)
-                #prefix = "label"
-                #add_children(release, element)
+                    release.get_labels().append(label_row)
+                    #print(release.get_labels())
             elif event == "start" and tag == "tracklist":
                 for track in element.iterchildren():
                     track_row = dict()
+                    release_tracks = release.get_tracks()
                     add_children(track_row, track)
                     track_row['release_id'] = release_id
-                    print(track_row)
                     release_tracks.append(track_row)
-
     return release
 
 def add_children(d, element):
@@ -47,7 +52,6 @@ def add_children(d, element):
         val = child.text
         if val is not None:
             d[prefix + "_" + key] = val
-    print(d)
 
 def add_attributes(d, element):
     prefix = element.tag
