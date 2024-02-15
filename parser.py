5a2b10d73a8805d18aea005f0d3f1e0f8a193881
sina <sinaastani@gmail.com>
2024-02-14 17:05:50 -0800

    model each release as object, add logic for labels

--- parser.py
+++ parser.py
@@ -1,6 +1,26 @@
 from lxml import etree
 from collections import deque
 
+class ReleaseObject():
+    def __init__(self, id, status):
+        self.id = id
+        self.status = status
+        self.artist_id = None
+        self.artist_name = None
+        self.labels = list()
+        self.tracks = list()
+
+    def set_artist(self, name, id):
+        self.artist_name = name
+        self.artist_id = id
+
+    def get_labels(self):
+        return self.labels
+
+    def get_tracks(self):
+        return self.tracks
+
+
 def parse_xml(file_name):
     context = etree.iterparse(file_name, events=('start', 'end'))
     #event, root = next(context)
@@ -10,35 +30,49 @@ def parse_xml(file_name):
         #if we have found "start" of release, build release object
         if event == "start" and tag == "release":
             release = dict()
-            release_tracks = dict()
+            release_tracks = list()
+            release_labels = list()
             release_id = element.get('id')
-            release['release_id'] = release_id
             release_status = element.get('status')
-            release['status'] = release_status
-            release_tracks['release_id'] = release_id
+            #release = ReleaseObject(release_id, release_status)
         else:
             if event == "start" and tag == "artists":
-                for child in element.iterchildren():
-                    add_children(release, child)
+                for artist in element.iterchildren():
+                    add_children(release, artist)
             elif event == "start" and tag == "labels":
-                event, element = next(context)
-                label_attributes = element.attrib
-                print(label_attributes)
+                for label in element.iterchildren():
+                    label_row = dict()
+                    add_attributes(label_row, label)
+                    print(label_row)
                 #get label element
                 #event, element = next(context)
                 #prefix = "label"
                 #add_children(release, element)
             elif event == "start" and tag == "tracklist":
-                for child in element.iterchildren():
-                    add_children(release_tracks, child)
+                for track in element.iterchildren():
+                    track_row = dict()
+                    add_children(track_row, track)
+                    track_row['release_id'] = release_id
+                    print(track_row)
+                    release_tracks.append(track_row)
 
     return release
 
-def add_children(obj, element):
+def add_children(d, element):
     #use "track", "artist", etc as prefix for key
     prefix = element.tag
     for child in element:
         key = child.tag
         val = child.text
         if val is not None:
-            obj[prefix + "_" + key] = val
+            d[prefix + "_" + key] = val
+    print(d)
+
+def add_attributes(d, element):
+    prefix = element.tag
+    attributes = element.attrib
+    for a in attributes:
+        val = attributes[a]
+        d[prefix + "_" + a] = val
+
+parse_xml('/Volumes/WD Black/Discogs/sample_release/release-3068908.xml')
