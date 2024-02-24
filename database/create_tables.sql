6d2bcd5aa7e607deba51224b6ca095322e9d36a7
sastani <sinaastani@gmail.com>
2024-02-23 21:48:34 -0800

    change sql code, seperate fks

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,6 +1,6 @@
 /* DDL FOR artist entities*/
 CREATE TABLE artists(
-    artist_id INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     artist_name VARCHAR,
     name VARCHAR,
     profile VARCHAR,
@@ -8,20 +8,27 @@ CREATE TABLE artists(
 );
 
 CREATE TABLE artist_name_variations(
-    artist_id INT references artists(artist_id),
+    artist_id INT,
     name VARCHAR,
     PRIMARY KEY(artist_id, name)
 );
 
 CREATE TABLE artist_aliases(
-    artist_id INT references artists(artist_id),
-    alias_id INT references artists(artist_id),
+    artist_id INT,
+    alias_artist_id INT,
     alias_name VARCHAR,
-    PRIMARY KEY(artist_id, alias_id)
+    PRIMARY KEY(artist_id, alias_artist_id)
 );
 
+CREATE TABLE artist_groups(
+    artist_id INT,
+    artist_name VARCHAR,
+    group_artist_id INT,
+    group_name VARCHAR
+)
+
 CREATE TABLE artist_urls(
-    artist_id INT references artists(artist_id),
+    artist_id INT,
     url VARCHAR,
     page_type VARCHAR,
     PRIMARY KEY(artist_id, url)
@@ -30,8 +37,8 @@ CREATE TABLE artist_urls(
 
 /* DDL FOR label entities*/
 CREATE TABLE labels(
-    label_id INT PRIMARY KEY,
-    label_name VARCHAR,
+    id INT PRIMARY KEY,
+    name VARCHAR,
     parent_label_id INT,
     parent_label_name VARCHAR,
     contact_info VARCHAR,
@@ -41,14 +48,14 @@ CREATE TABLE labels(
 
 
 CREATE TABLE label_sub_labels(
-    label_id INT references labels(label_id),
-    sub_label_id INT references labels(label_id),
-    sub_label_name VARCHAR,
-    PRIMARY KEY (label_id, sub_label_id)
+    label_id INT references labels(id),
+    sub_label_label_id INT references labels(id),
+    sub_label_label_name VARCHAR,
+    PRIMARY KEY (label_id, sub_label_label_id)
 );
 
 CREATE TABLE label_urls(
-    label_id INT references labels(label_id),
+    label_id INT references labels(id),
     url VARCHAR,
     page_type VARCHAR,
     PRIMARY KEY(label_id, url)
@@ -58,20 +65,18 @@ CREATE TABLE label_urls(
 /* DDL FOR release entities*/
 CREATE TABLE releases
 (
-    release_id INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     master_id INT,
     title VARCHAR NOT NULL,
     release_date DATE,
-    label_id INT,
-    label_name VARCHAR,
     status VARCHAR,
     data_quality VARCHAR
 );
 
 CREATE TABLE release_artists
 (
-    release_id  INT references releases (release_id),
-    artist_id   INT references artists (artist_id),
+    release_id  INT,
+    artist_id   INT,
     artist_name VARCHAR,
     ordinal INT,
     join_string VARCHAR,
@@ -80,21 +85,21 @@ CREATE TABLE release_artists
 
 CREATE TABLE release_genres
 (
-    release_id INT references releases(release_id),
+    release_id INT,
     genre VARCHAR,
     PRIMARY KEY (release_id, genre)
 );
 
 CREATE TABLE release_styles
 (
-    release_id INT references releases(release_id),
+    release_id INT,
     style VARCHAR,
     PRIMARY KEY (release_id, style)
 );
 
 CREATE TABLE release_tracks
 (
-    release_id INT references releases(release_id),
+    release_id INT,
     track_title VARCHAR,
     track_number INT,
     duration  TIME,
@@ -103,8 +108,8 @@ CREATE TABLE release_tracks
 
 CREATE TABLE release_labels
 (
-    release_id INT references releases(release_id),
-    label_id INT references labels(label_id),
+    release_id INT,
+    label_id INT,
     label_name VARCHAR,
     catalog_num VARCHAR,
     PRIMARY KEY(release_id, label_id)
@@ -112,7 +117,7 @@ CREATE TABLE release_labels
 
 CREATE TABLE release_formats
 (
-    release_id INT references releases(release_id),
+    release_id INT references releases(id),
     format VARCHAR,
     quantity INT,
     description_arr VARCHAR[],
@@ -123,11 +128,6 @@ CREATE TABLE release_formats
 CREATE TABLE master_releases
 (
     master_id INT PRIMARY KEY,
-    release_id INT references releases(release_id)
-);
-
-CREATE TABLE master_main_release
-(
-    master_id INT PRIMARY KEY,
-    main_release_id INT references releases(release_id)
+    release_id INT,
+    main_release BOOLEAN
 );
