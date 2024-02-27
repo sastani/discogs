9d3364ff6dacce6c4f1ac51bff81c4e800f06d21
sastani <sinaastani@gmail.com>
2024-02-27 15:56:42 -0800

    separate PK constrains from CREATE statements, sql for dropping tables

---
+++ database/create_pks.sql
@@ -0,0 +1,11 @@
+--might fail due to discogs duplicates
+ALTER TABLE release_artists ADD CONSTRAINT release_artists_pk PRIMARY KEY (release_id, artist_id, ordinal);
+ALTER TABLE release_tracks ADD CONSTRAINT release_tracks_pk PRIMARY KEY(release_id, track_title, track_number);
+ALTER TABLE release_labels ADD CONSTRAINT release_labels_pk PRIMARY KEY(release_id, label_id);
+
+--will very likely fail (almost 100%)
+ALTER TABLE release_styles ADD CONSTRAINT release_styles_pk PRIMARY KEY (release_id, style);
+ALTER TABLE release_formats ADD CONSTRAINT release_formats_pk PRIMARY KEY (release_id, format, text, description_arr);
+
+
+
---
+++ database/drop_tables.sql
@@ -0,0 +1,31 @@
+DROP TABLE artists;
+
+DROP TABLE artist_name_variations;
+
+DROP TABLE artist_aliases;
+
+DROP TABLE artist_groups;
+
+DROP TABLE artist_urls;
+
+DROP TABLE label_sub_labels;
+
+DROP TABLE label_urls;
+
+DROP TABLE labels;
+
+DROP TABLE release_artists;
+
+DROP TABLE release_genres;
+
+DROP TABLE release_styles;
+
+DROP TABLE release_tracks;
+
+DROP TABLE release_labels;
+
+DROP TABLE release_formats;
+
+DROP TABLE releases;
+
+DROP TABLE release_master;
