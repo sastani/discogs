20ce9e7539d6556d068d4cc03190ee9bdd40878c
sastani <sinaastani@gmail.com>
2024-02-28 11:38:25 -0800

    fix convert_duration() to be fault tolerant

--- models/utils.py
+++ models/utils.py
@@ -1,7 +1,7 @@
 import re
 def cleanup_duration(id, duration):
-    valid_patterns = ['\d{2}:\d{2}:\d{2}', '\d{1}:\d{2}:\d{2}', '\d{2}:\d{2}', '\d{2}:\d{1}', '\d{1}:\d{2}', '\d{1}:\d{1}']
-    alternative_patterns = ['\d{2}\.\d{2}', '\d{1}\.\d{2}']
+    valid_patterns = ['^\d{2}:\d{2}:\d{2}$', '^\d{1}:\d{2}:\d{2}$', '^\d{2}:\d{2}$', '^\d{2}:\d{1}$', '^\d{1}:\d{2}$', '^\d{1}:\d{1}$']
+    alternative_patterns = ['^\d{2}\.\d{2}$', '^\d{1}\.\d{2}$']
     valid_duration = False
     alt_duration = False
     duration_parts = None
@@ -20,24 +20,32 @@ def cleanup_duration(id, duration):
             duration_parts = duration.split('.')
     return duration_parts
 
-def convert_duration(duration_as_list):
+def convert_duration(id, log, duration, duration_as_list):
     num_places = len(duration_as_list)
-    if num_places == 1:
-        seconds = int(duration_as_list[0])
-        minutes = 0
-        hours = 0
-    elif num_places == 2:
-        minutes, seconds = map(int, duration_as_list)
-        hours = 0
-    elif num_places == 3:
-        hours, minutes, seconds = map(int, duration_as_list)
-    else:
+    try:
+        if num_places == 1:
+            seconds = int(duration_as_list[0])
+            minutes = 0
+            hours = 0
+        elif num_places == 2:
+            minutes, seconds = map(int, duration_as_list)
+            hours = 0
+        elif num_places == 3:
+            hours, minutes, seconds = map(int, duration_as_list)
+        else:
+            return None, None, None
+        mins_to_add = seconds // 60
+        seconds = seconds % 60
+        hours_to_add = (minutes + mins_to_add) // 60
+        minutes = (minutes + mins_to_add) % 60
+        hours = hours + hours_to_add
+        if hours >= 24:
+            return None, None, None
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("convert_duration() FAILED for release: " + id + " with duration str: " + duration + " duration list: ", duration_as_list)
+        log.write("convert_duration() FAILED for release: " + id + " with duration str: \"" + duration + "\"")
         return None, None, None
-    mins_to_add = seconds // 60
-    seconds = seconds % 60
-    hours_to_add = minutes // 60
-    minutes = (minutes % 60) + mins_to_add
-    hours = hours + hours_to_add
     return hours, minutes, seconds
 
 def get_row(fields, values, test):
