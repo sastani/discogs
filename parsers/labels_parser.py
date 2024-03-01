308f06eb8520342c6374659cf37024cd05b3c63b
sastani <sinaastani@gmail.com>
2024-03-01 02:45:44 -0800

    simpler/updated parser which gets all labels

--- parsers/labels_parser.py
+++ parsers/labels_parser.py
@@ -1,41 +1,24 @@
 from lxml import etree
 from models.label import Label
 from database.loaders.export import Exporter
-
 def parse_xml(file_name):
-    context = etree.iterparse(file_name, events=('start', 'end'))
+    #context = etree.iterparse(file_name, events=('end',))
     #advance iterator to root element
-    event, root = next(context)
-    context = etree.iterwalk(root, events=('start','end'))
+    #event, root = next(context)
+    #context = etree.iterwalk(context, events=('end',))
     #advance iterator to release/child of root
-    next(context)
     E = Exporter()
     all_labels = list()
-
+    context = etree.iterparse(file_name, tag="label")
     for event, element in context:
-        tag = element.tag
-        label = None
-        # if we have found "start" of label, build label object
-        if event == "start" and tag == "label":
-            label_id = None
-            label_name = None
-            for child in element.iterchildren():
-                text = child.text
-                tag = child.tag
-                if tag == "images":
-                    continue
-                elif tag == "id":
-                    label_id = text
-                elif tag == "name":
-                    label_name = text
-                else:
-                    break
-            if label_id and label_name:
-                #create label object once id and name found
-                label = Label(label_id, label_name)
-            for child in element.iterchildren():
-                text = child.text
-                tag = child.tag
+        i = find_id(element)
+        n = find_name(element)
+        if i is not None and n is not None:
+            label = Label(i, n)
+            walk_context = etree.iterwalk(element, events=('end',))
+            for event_label, label_element in walk_context:
+                tag = label_element.tag
+                text = label_element.text
                 if tag == "contactinfo":
                     label.set_contact_info(text)
                 elif tag == "profile":
@@ -43,25 +26,32 @@ def parse_xml(file_name):
                 elif tag == "data_quality":
                     label.set_quality(text)
                 elif tag == "parentlabel":
-                    parent_label_id = child.get('id')
+                    parent_label_id = element.get('id')
                     label.set_parent_label(parent_label_id, text)
                 elif tag == "urls":
                     label_urls = label.get_urls()
-                    for url in child:
+                    for url in element:
                         url_path = url.text
                         if url_path:
                             label_urls.append(url_path)
                 elif tag == "sublabels":
                     label_sub_labels = list()
-                    for sub_label in child:
+                    for sub_label in element:
                         sub_label_row = sub_label.attrib
                         sub_label_name = sub_label.text
-                        sub_label_row['name'] = sub_label_name
-                        label_sub_labels.append(sub_label_row)
+                        if sub_label_row:
+                            sub_label_row['name'] = sub_label_name
+                            label_sub_labels.append(sub_label_row)
                     label.set_sub_labels(label_sub_labels)
-        if label:
             all_labels.append(label)
-        context.skip_subtree()
-    if all_labels:
-        E.load_all(all_labels, "label")
-        context.skip_subtree()
+    return all_labels
+
+def find_id(element):
+    i = element.find('id')
+    if i is not None:
+        return i.text
+
+def find_name(element):
+    n = element.find('name')
+    if n is not None:
+        return n.text
